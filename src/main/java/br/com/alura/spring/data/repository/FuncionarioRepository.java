eb9b4a6cbbd8db97a56b9f1688a8eaa5d267ef69
Leny <53267608+rymerki@users.noreply.github.com>
2020-12-09 16:23:59 -0300

    Fazendo projeção de uma classe

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
 @Repository
 public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
@@ -17,7 +18,11 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 			+ "AND f.salario >= :salario AND f.dataContratacao = :data")
 	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
 	
-	@Query(value= "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data",
+	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data",
 			nativeQuery = true)
 	List<Funcionario> findDataContratacaoMaior(LocalDate data);
+	
+	@Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", 
+			nativeQuery = true)
+	List<FuncionarioProjecao> findFuncionarioSalario();
 }
--- src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
+++ src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 
 @Service
@@ -30,6 +31,7 @@ public class RelatoriosService {
 			System.out.println("1 - busca funcionario nome");
 			System.out.println("2 - busca funcionario nome, data contratacao e salario maior");		
 			System.out.println("3 - busca funcionario data contratacao");
+			System.out.println("4 - pesquisa funcionario salario");
 			
 			int action = scanner.nextInt();
 			
@@ -42,6 +44,9 @@ public class RelatoriosService {
 				break;
 			case 3:
 				buscaFuncionarioDataContratacao(scanner);
+				break;
+			case 4:
+				pesquisaFuncionarioSalario();
 				break;
 			default:
 				system = false;
@@ -79,5 +84,10 @@ public class RelatoriosService {
 		
 		List<Funcionario> list = funcionarioRepository.findDataContratacaoMaior(localDate);
 		list.forEach(System.out::println);
+	}
+	
+	private void pesquisaFuncionarioSalario() {
+		List<FuncionarioProjecao> list = funcionarioRepository.findFuncionarioSalario();
+		list.forEach(f -> System.out.println("Fuincionario: id: " +f.getId() + " | nome: " + f.getNome() + " | salario: " + f.getSalario()));
 	}
 }
